cc4c00408354d7e96632d28c87427f616d7af6e5
Alexey Lupenkov <alupenkov@inbox.ru>
2025-02-25 18:14:27 +0300

    добавил модель session

---
+++ model/session.go
@@ -0,0 +1,12 @@
+package model
+
+import "time"
+
+type Session struct {
+	ID     string    `json:"username"`
+	Expiry time.Time `json:"expiry"`
+}
+
+func (s Session) IsExpired() bool {
+	return s.Expiry.Before(time.Now())
+}
