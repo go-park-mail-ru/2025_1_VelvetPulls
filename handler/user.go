8a56f64e72922572cb90f7090036f438b6d04383
Alexey Lupenkov <alupenkov@inbox.ru>
2025-02-25 21:44:55 +0300

    добавил документацию для ручки регистрации

--- handler/user.go
+++ handler/user.go
@@ -8,6 +8,17 @@ import (
 	utils "github.com/go-park-mail-ru/2025_1_VelvetPulls/utils"
 )
 
+// Register регистрирует нового пользователя
+// @Summary Регистрация нового пользователя
+// @Description Регистрирует нового пользователя по данным, переданным в запросе
+// @Tags User
+// @Accept json
+// @Produce json
+// @Param user body model.User true "Данные для регистрации пользователя"
+// @Success 201 {object} model.User
+// @Failure 400
+// @Failure 500
+// @Router /api/register [post]
 func Register(w http.ResponseWriter, r *http.Request) {
 	var user model.User
 
