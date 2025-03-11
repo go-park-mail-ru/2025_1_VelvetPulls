8c022a5b07a15da038e0d1cd766c3619278aa227
ruslan <runovikov2@gmail.com>
2025-03-11 15:33:09 +0300

    fixed error handling

--- service/chat.go
+++ service/chat.go
@@ -1,7 +1,6 @@
 package service
 
 import (
-	"github.com/go-park-mail-ru/2025_1_VelvetPulls/apperrors"
 	"github.com/go-park-mail-ru/2025_1_VelvetPulls/model"
 	"github.com/go-park-mail-ru/2025_1_VelvetPulls/repository"
 )
@@ -9,17 +8,11 @@ import (
 func FetchChatsBySession(token string) ([]*model.Chat, error) {
 	session, err := repository.GetSessionBySessId(token)
 	if err != nil {
-		if err == apperrors.ErrSessionNotFound {
-			return nil, apperrors.ErrSessionNotFound
-		}
 		return nil, err
 	}
 
 	chats, err := repository.GetChatsByUsername(session.Username)
 	if err != nil {
-		if err == apperrors.ErrChatNotFound {
-			return nil, apperrors.ErrChatNotFound
-		}
 		return nil, err
 	}
 
