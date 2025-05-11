b4199e240486234a515ce7d585dd4ee5f6d80410
Alexey Lupenkov <alupenkov@inbox.ru>
2025-05-11 13:22:12 +0300

    final hw2

--- db/migrations/user.sql
+++ db/migrations/user.sql
@@ -1,5 +1,4 @@
--- Замена "your_database" на имя вашей базы данных
 GRANT CONNECT ON DATABASE mydb TO user_for_chat; -- Доступ на подключение к бд.
-GRANT USAGE ON SCHEMA public TO user_for_chat;           -- Доступ к схеме (по умолчанию public)
+GRANT USAGE ON SCHEMA public TO user_for_chat;   -- Доступ к схеме
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO user_for_chat; --Доступ на чтение и запись всех таблиц
